3954668de072855901d4fefa183dcfe8e9ba39d2
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-06-07 23:44:20 +0300

    up

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,7 +33,7 @@ class AdminPanelProvider extends PanelProvider
             ->profile()
             ->brandName('I Quran')
             ->colors([
-                'primary' => Color::Green,
+                'primary' => Color::Amber,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
